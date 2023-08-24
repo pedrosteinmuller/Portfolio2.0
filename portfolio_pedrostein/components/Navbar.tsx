55dda488e2183a4ca9e152e4bb01bf09081daf2c
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2023-08-24 10:43:28 -0300

    feat: continuation of navbar component and adding fmotion framework to animate some features

--- portfolio_pedrostein/components/Navbar.tsx
+++ portfolio_pedrostein/components/Navbar.tsx
@@ -1,31 +1,97 @@
-import { logo } from '@/public/assets'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { logo } from '@/public/assets';
+import Image from 'next/image';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   return (
     <div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4'>
-      
+
       <div className='max-w-container h-full mx-auto py-1 font-titleFont flex item-center justify-between'>
-        <div>
-          <Image className='w-24' src={logo} alt='logo' />
-        </div>
-        <div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+        >
+          <Image className='w-24 mt-6' src={logo} alt='logo' />
+        </motion.div>
+        <div className='hidden mdl:inline-flex items-center gap-7'>
           <ul className='flex text-[15px] gap-8'>
             <Link
-            href="#home"
-            className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+              href="#home"
+              className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
             >
-            <li>Página Inicial</li>
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1 }}
+              >
+                Página Inicial
+              </motion.li>
             </Link>
             <Link
-            href="#home"
-            className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+              href="#about"
+              className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
             >
-            <span>01.</span> Sobre mim
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.1 }}
+              >
+                <span className='text-textGreen'>01.</span>
+                Sobre mim
+              </motion.li>
+            </Link>
+            <Link
+              href="#project"
+              className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.2 }}
+              >
+                <span className='text-textGreen'>02.</span>
+                Projetos
+              </motion.li>
+            </Link>
+            <Link
+              href="#experience"
+              className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.3 }}
+              >
+                <span className='text-textGreen'>03.</span>
+                Experiências
+              </motion.li>
+            </Link>
+            <Link
+              href="#contact"
+              className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.4 }}
+              >
+                <span className='text-textGreen'>04.</span>
+                Contato
+              </motion.li>
             </Link>
           </ul>
+          <a href='/assets/pedro_curriculo.pdf' target='_blank'>
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.5 }}
+              className='px-4 py-2 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300'
+            >
+              Currículo
+            </motion.button>
+          </a>
         </div>
       </div>
     </div>
